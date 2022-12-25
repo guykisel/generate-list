d028d2d1067cce4ef61c3ae937ff7b7b6d55c9b1
Guy Kisel <guy.kisel@gmail.com>
2022-12-24 21:49:09 -0800

    write some code

---
+++ generate_list/__about__.py
@@ -0,0 +1,3 @@
+# hatch is configured in pyproject.toml to get the version from here
+# don't edit this manually, just use hatch to bump the version
+__version__ = "0.0.0"
--- generate_list/main.py
+++ generate_list/main.py
@@ -1,7 +1,14 @@
+import pathlib
+
+import nestedtext
 import typer
 
 
-def main():
+def main(path: pathlib.Path = pathlib.Path("../links/links.nt")):
+    with open(path) as f:
+        links = nestedtext.load(f, top="dict")
+        typer.echo(links)
+        typer.echo(links["Jenkins CI"]["url"])
     print("Hello World")
 
 
